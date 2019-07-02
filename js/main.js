70a6912d3037aac25713f7bb75fb96d24ad20cf2
Ibrahim-sakaama <barhouma.99@gmail.com>
2019-07-02 17:57:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function strongg()
+{
+    var  bold_Items = document.getElementById("textt");
+    if (bold_Items.style.fontWeight=="normal"){
+        bold_Items.style.fontWeight="bold";
+    }
+    else {
+        bold_Items.style.fontWeight="normal";
+    }
+
+}
+
+function italicc() 
+{
+    var  italic_Items = document.getElementById("textt");
+    if(italic_Items.style.fontStyle=="normal"){
+    italic_Items.style.fontStyle="italic";
+    }
+    else {
+        italic_Items.style.fontStyle="normal"
+    }
+}
+
+function underlinedd() 
+{
+    var  underlined_Items = document.getElementById("textt");
+    if(underlined_Items.style.textDecoration=="none"){
+    underlined_Items.style.textDecoration="underline";
+    }
+    else{
+        underlined_Items.style.textDecoration="none"
+    }
+}
+
+function twenty()
+{
+    var selected=document.getElementById("select").value;
+    document.getElementById("textt").style.fontSize=selected;
+}
+
+function thirty()
+{
+    var selectedd=document.getElementById("selectt").value;
+    document.getElementById("textt").style.fontFamily=selectedd;
+}
